0275e082d7bc3eb8cb537fab7e1b03fb015c3dcc
DavifvnunesUnicap <davi.00000857988@unicap.br>
2026-02-19 10:15:45 -0300

    atualizando lista

--- Estrutura_de_Dados/lista_main.c
+++ Estrutura_de_Dados/lista_main.c
@@ -3,10 +3,17 @@
 #define TAM 5
 
 int main(){
+    Quadrado lista[TAM];
     float lado;
 
+
     for(int i = 0; i > TAM; i++){
-        printf("Insira o Quadrado")
+        printf("Insira o lado do Quadrado %d:\n", i+1);
+        scanf("%n", &lado);
+    
+        lista[i] = cria_quadrado(lado);
     }
     
+    
+    return 0;
 }
